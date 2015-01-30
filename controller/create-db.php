e73e781d64e0980e7964d6bdb4aaf3f0c16f2224
kristopher jones <grofernioken@gmail.com>
2015-01-30 14:02:32 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,10 +1,17 @@
 <?php
     require_once(__DIR__ . "/../model/database.php");
     $connection = new mysqli($host, $username, $password);
-    if($connection->connect-error) {
-        die("error: " . $connection->connect-error);
+    if($connection->connect_error) {
+        die("error: " . $connection->connect_error);
+    }
+    $exists = $connection->select_db($database);
+    if(!$exists) {
+        $query = $connection->query("CREATE DATABASE $database");
+        if($query) {
+            echo "successfully created database: " . $database;
+        }
     }
     else {
-        echo "success: " . $connection->host_info;
+        echo "database already exists";
     }
     $connection->close();
