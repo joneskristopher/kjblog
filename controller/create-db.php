eda6755e4969fbaa0564ae70335cf5b2e1f7d604
kristopher jones <grofernioken@gmail.com>
2015-02-04 13:58:57 -0800

    hi alex

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,25 +2,25 @@
     require_once(__DIR__ . "/../model/database.php");
     $connection = new mysqli($host, $username, $password);
     if($connection->connect_error) {
-        die("error: " . $connection->connect_error);
+        die("<p>error: " . $connection->connect_error . "</p>");
     }
     $exists = $connection->select_db($database);
     if(!$exists) {
         $query = $connection->query("CREATE DATABASE $database");
         if($query) {
-            echo "successfully created database: " . $database;
+            echo "<p>successfully created database: " . $database . "</p>";
         }
     }
     else {
-        echo "database already exists";
+        echo "<p>database already exists</p>";
     }
     $query = $connection->query("CREATE TABLE posts ("
             . "id int(11) NOT NULL AUTO_INCREMENT,"
-            . "title vachar(255) NOT NULL,"
+            . "title varchar(255) NOT NULL,"
             . "post text NOT NULL,"
             . "PRIMARY KEY (id))");
     if($query) {
-        echo "successfully created table: posts";
+        echo "<p>successfully created table: posts</p>";
     }
     else {
         echo "<p>$connection->error</p>";
---
+++ view/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+    <u1>
+        <li><a href="/kjblog/post.php">blog post form</a></li>
+    </u1>
+</nav>
